17bd103fc3202d1323b7533e49ecac57036d48b3
wangzh <www.2314990274@qq.com>
2024-01-23 17:56:58 +0800

    feat:添加导航栏

--- react-admin/src/layout/aside.jsx
+++ react-admin/src/layout/aside.jsx
@@ -1,13 +1,28 @@
-import { Layout } from 'antd';
+import { Layout, Menu } from 'antd';
 import { menuList } from  '../stores'
+import { useMemo } from 'react';
+import {  MailOutlined } from '@ant-design/icons';
 
 const { Header, Footer, Sider, Content } = Layout;
 
 const MyLayoutAside=(props)=>{
     console.log(menuList);
+
+    const getMenuItem=(item,lv)=>{
+        const { name, routeName, children } = item
+        const Icon = <MailOutlined/>
+        if (empty(children)){
+            getMenuItem(children,lv+1)
+        }
+    }
+
+    const items = useMemo(() => {
+        return menuList.map(item => getMenuItem(item,0))
+    }, [menuList])
+
     return <>
         <Sider className='my-aside'>
-            <div>MyLayoutAside</div>
+            <Menu items={items}/>
         </Sider>
     </>
 
