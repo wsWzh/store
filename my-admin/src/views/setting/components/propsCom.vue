324dc48e9c8183982b993f205739ac047ff945ed
wangzh <www.2314990274@qq.com>
2024-02-01 09:22:26 +0800

    feat:添加案例

---
+++ my-admin/src/views/setting/components/propsCom.vue
@@ -0,0 +1,26 @@
+<script lang="jsx">
+export default {
+    name: '',
+    emits: [],
+    props: {},
+    setup(props, { attrs, slots, emit }) {
+
+        const _slots   = slots.default()[0]
+
+        const _onClick   =  ()  => {
+            console.log('内置的click');
+        }
+
+
+        if  (_slots.props.onClick ) {
+                _slots.props.onClick=[_slots.props.onClick,_onClick]
+        }
+
+        Object.assign(_slots,  {})
+
+        return () => {
+            return _slots
+        }
+    }
+}
+</script>
